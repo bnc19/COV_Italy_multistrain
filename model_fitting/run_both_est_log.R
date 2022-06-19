57e937ff4c9ab1b28fd5f84f6f6c9ef86466748d
Bethan Cracknell Daniels <66028934+bnc19@users.noreply.github.com>
2022-06-19 16:05:00 +0100

    add script to run log models

--- model_fitting/run_both_est_log.R
+++ model_fitting/run_both_est_log.R
@@ -22,27 +22,26 @@ obj <- didehpc::queue_didehpc(ctx, config)
 
 source("R/run_model_fitting.R")
 source("R/diagnose_stan_fit.R")
-obj$task_get("8ba38475681fb6d61b3fb9fa560e369f")$log()
+obj$task_get("dc7c8d55baf40bc205d9b9a3849daa2e")$log()
 
 
 # Global parameters ------------------------------------------------------------
 
 n_iter = 1000
 n_warmups = round(n_iter/2)
-scale_time_step = 5
+scale_time_step = 6
 
 
-dir.create(paste0("Results/not_log"))
-dir.create(paste0("Results/not_log/scale", scale_time_step))
-dir.create(paste0("Results/not_log/scale", scale_time_step,"/inc"))
-dir.create(paste0("Results/not_log/scale", scale_time_step,"/prev"))
+dir.create(paste0("Results/scale", scale_time_step))
+dir.create(paste0("Results/scale", scale_time_step,"/inc"))
+dir.create(paste0("Results/scale", scale_time_step,"/prev"))
 
 
 ############################# Fit models to incidence ##########################
-file_path_inc = paste0("Results/not_log/scale", scale_time_step, "/inc")
+file_path_inc = paste0("Results/scale", scale_time_step, "/inc")
 
 # Run model with testing of symptomatic only -----------------------------------
-test_symp =  obj$enqueue(
+test_symp2 =  obj$enqueue(
   run_model_fitting(
     file_path=  paste0(file_path_inc,"/symp_test"),
     model_path = "model/est_test_symp.stan",
@@ -54,9 +53,13 @@ test_symp =  obj$enqueue(
 )
 
 test_symp$log()
+# f97022895f5d62f9139c71ac39ae279a
+
+test_symp2 $log()
+# b7f60fceb3d5d394ba52c03e1214eb41
 
 # Run model with testing of asymptomatic plus all symptomatic isolate ----------
-test_asymp =  obj$enqueue(
+test_asymp2 =  obj$enqueue(
   run_model_fitting(
     file_path= paste0(file_path_inc,"/asymp_test"),
     model_path = "model/est_test_asymp.stan", 
@@ -69,9 +72,14 @@ test_asymp =  obj$enqueue(
 
 
 test_asymp$log()
+# 49ea207c56135704f5df31f8c4a89263
+
+test_asymp2$log()
+
+# d57a018949494a880ae5e0cec30421a0
 
 # Run model with same level of asymp and symp testing --------------------------
-test_asymp_and_symp =  obj$enqueue(
+test_asymp_and_symp2 =  obj$enqueue(
   run_model_fitting(
     file_path= paste0(file_path_inc,"/asymp_and_symp_test"),
     model_path = "model/est_test_asymp_and_symp.stan",
@@ -84,7 +92,11 @@ test_asymp_and_symp =  obj$enqueue(
 
 
 test_asymp_and_symp$log()
+# d864226d68bb0504b3a1565a2ce71387
 
+
+test_asymp_and_symp2$log()
+# a089e577aa8e7e6730a7af89ce08c835
 
 
 ############################# Fit models to prevalence ##########################
@@ -99,7 +111,7 @@ pars = c("lp__",
          "I0_ven[1]", "I0_ven[2]","I0_ven[3]","I0_ven[4]",
          "k", "tau")
 
-file_path_prev  = paste0("Results/not_log/scale", scale_time_step, "/prev")
+file_path_prev  = paste0("Results/scale", scale_time_step, "/prev")
 
 
 
@@ -108,7 +120,7 @@ file_path_prev  = paste0("Results/not_log/scale", scale_time_step, "/prev")
 test_symp_prev=  obj$enqueue(
   run_model_fitting(
     file_path= paste0(file_path_prev,"/symp_test"),
-    model_path = "model/est_test_symp_prev.stan",
+    model_path = "model/log/est_test_symp_prev.stan",
     n_iter = n_iter,
     n_warmups = n_warmups,
     pars = pars,
@@ -120,9 +132,11 @@ test_symp_prev=  obj$enqueue(
 
 test_symp_prev$log()
 
+# 8ba38475681fb6d61b3fb9fa560e369f bad priors?
+
 ## diag 
 
-fit_symp = obj$task_get("b42507cb0fd2ea7eed154f41f389742e")$result()
+fit_symp = obj$task_get("8ba38475681fb6d61b3fb9fa560e369f")$result()
 
 symp_diag = 
   obj$enqueue(diagnose_stan_fit(
@@ -139,7 +153,7 @@ symp_diag$log()
 test_asymp_prev =  obj$enqueue(
   run_model_fitting(
     file_path= paste0(file_path_prev,"/asymp_test"),
-    model_path = "model/est_test_asymp_prev.stan",
+    model_path = "model/log/est_test_asymp_prev.stan",
     n_iter = n_iter,
     n_warmups = n_warmups,
     pars = pars,
@@ -153,16 +167,18 @@ test_asymp_prev =  obj$enqueue(
 
 test_asymp_prev$log()
 
+# 3fb8138800fd7d9c6493496f047ad126 bad priors?
+
 ## diag  
 
 
-fit_asymp =  obj$task_get("cff8091ccbad7869c9bc95a0d5df284a")$result()
+fit_asymp =  obj$task_get("fba3228c9bfd82a3787b82627a74a878")$result()
 
 asymp_diag = 
   obj$enqueue(diagnose_stan_fit(
   fit =fit_asymp ,
   pars = pars,
-  file_path = paste0(file_path_prev, "/asymp_test")
+  file_path = paste0(file_path_prev, "LN_tau/asymp_test")
 ))
 
 asymp_diag$log()
@@ -172,7 +188,7 @@ asymp_diag$log()
 test_asymp_and_symp_prev =  obj$enqueue(
   run_model_fitting(
     file_path= paste0(file_path_prev,"/asymp_and_symp_test"),
-    model_path = "model/est_test_asymp_and_symp_prev.stan",
+    model_path = "model/log/est_test_asymp_and_symp_prev.stan",
     n_iter = n_iter,
     n_warmups = n_warmups,
     pars = pars,
@@ -185,11 +201,13 @@ test_asymp_and_symp_prev =  obj$enqueue(
 
 test_asymp_and_symp_prev$log()
 
+# 3ad50ef56b49609752266a40672cc55c bad priors?
 
+obj$task_get("341c2494c602d68b14ae40e4eb2d42f3")$log()
 ## diag 
+rstan::check_divergences(fit_asymp_and_symp)
 
-
-fit_asymp_and_symp =obj$task_get("3f13a3f65869f8f508e9a7a37a2c89f5")$result()
+fit_asymp_and_symp =test_asymp_and_symp_prev$result()
 
 asymp_symp_diag = 
   obj$enqueue(diagnose_stan_fit(
